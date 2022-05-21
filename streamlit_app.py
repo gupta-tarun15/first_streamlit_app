6d7b1111fde5c4a30eeb7b975469d71170f69995
gupta-tarun15 <93879313+gupta-tarun15@users.noreply.github.com>
2022-05-21 18:01:13 +0530

    Update streamlit_app.py
    Corrected Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import streamlit
 streamlit.title('First Streamlit app')
 
 streamlit.header("Breakfast Menu")
-streamlit.text('🥣  🥗  🐔  🥑  🍞')
-streamlit.text('Sandwitch')
-streamlit.text('Burger')
-streamlit.text('Hard-Boiled Egg')
+streamlit.text('        ')
+streamlit.text('🥣 - Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 - Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 - Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 - Avacardo Toast')
+
